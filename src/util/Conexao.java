bf9af9643badf0d37ec0611f5fd7c7d2aba5b21c
kariokax <felipe.c.machado123@gmail.com>
2019-03-26 19:33:50 -0300

    Pacote Util, Biblioteca postgress

---
+++ src/util/Conexao.java
@@ -0,0 +1,46 @@
+package util;
+/**
+ *
+ * @author Heuber
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexao {
+
+    private static Connection conexao;
+
+    private static Connection conectar() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://localhost/dbbancario", "postgres", "123456");
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger("Driver JDBC não encontrado! Verifique as bibliotecas"
+                    + " da aplicação.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger("Caminho, banco de dados, usuário ou senha incorretos. Verifique!");
+        }
+        return null;
+    }
+
+    public static Connection getConexao() {
+        try {
+            while (conexao == null || conexao.isClosed()) {
+                Logger.getLogger("Tentando conexão...");
+                conexao = conectar();
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(e.getMessage());
+        }
+        
+        return conexao;
+    }
+}
+
