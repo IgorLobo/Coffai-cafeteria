594187a1ee0c941c8cd5fc746367cd061db16374
Igor Lobo <lobo.igor19@gmail.com>
2019-04-17 21:40:40 -0300

    Base dos JDialogsCapturarEntradas

---
+++ src/apresentacao/InternalFrameComponentesDoCafe.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apresentacao;
+
+/**
+ *
+ * @author Igor Lobo
+ */
+public abstract class InternalFrameComponentesDoCafe extends javax.swing.JInternalFrame{
+    public float porcentagem;
+    public String texto;
+    
+}
